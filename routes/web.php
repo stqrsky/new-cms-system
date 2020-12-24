98f36ccb4bfc142f7413dac78b9064999899397a
Starsky <starsky.m@live.com>
2020-12-24 22:18:24 +0100

    admin view creation setip

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
