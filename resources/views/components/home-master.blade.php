13311075834ac43b4698dbe3c92ff010823da71d
Starsky <starsky.m@live.com>
2020-12-25 04:25:49 +0100

    menu links, migration

--- resources/views/components/home-master.blade.php
+++ resources/views/components/home-master.blade.php
@@ -36,6 +36,21 @@
               <span class="sr-only">(current)</span>
             </a>
           </li>
+
+          @if(Auth::check())
+
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('admin.index')}}">Admin</a>
+          </li>
+
+          @else
+
+          <li class="nav-item">
+            <a class="nav-link" href="/login">Login</a>
+          </li>
+
+          @endif
+
           <li class="nav-item">
             <a class="nav-link" href="#">About</a>
           </li>
