9cdb962f319aea79ac159afbe3adf5033d3128c0
Starsky <starsky.m@live.com>
2021-01-02 12:28:23 +0100

    profile part2

--- resources/views/admin/users/profile.blade.php
+++ resources/views/admin/users/profile.blade.php
@@ -4,6 +4,56 @@
 
         <h1>User Profile for : {{$user->name}}</h1>
 
+        <div class="row">
+            <div class="col-sm-6">
+                <form method="post" action="" enctype="multipart/form-data">
+                    @csrf
+
+                    <div class="mb-4">
+                        <img class="img-profile rounded-circle"  height="50px" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="file">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text"
+                                name="name" 
+                                class="form-control" 
+                                id="name" 
+                                value="{{$user->name}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="text"
+                                name="email" 
+                                class="form-control" 
+                                id="email" 
+                                value="{{$user->email}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password"
+                                name="password" 
+                                class="form-control" 
+                                id="password" 
+                                >
+                    </div>
+                    <div class="form-group">
+                        <label for="password_confirmation">Confirm Password</label>
+                        <input type="password"
+                                name="password_confirmation" 
+                                class="form-control" 
+                                id="password_confirmation" 
+                                >
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+
     @endsection
 
 </x-admin-master>
