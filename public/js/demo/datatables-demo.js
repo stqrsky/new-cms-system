44d2442ce2312a25c70c7c393ce6cd0090c0de33
Starsky <starsky.m@live.com>
2021-01-02 23:23:01 +0100

    display all users part2/top avatar

--- public/js/demo/datatables-demo.js
+++ public/js/demo/datatables-demo.js
@@ -1,4 +1,7 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
   $('#dataTable').DataTable();
+
+  // call the id="users-table" from users/index.blade.php
+  $('#users-table').DataTable();
 });
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -4,6 +4,63 @@
 
         <h1>Users</h1>
 
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="users-table" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Username</th>
+                                <th>Avatar</th>
+                                <th>Name</th>
+                                <th>Registered date</th>
+                                <th>Updated profile date</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th>Id</th>
+                                <th>Username</th>
+                                <th>Avatar</th>
+                                <th>Name</th>
+                                <th>Registered date</th>
+                                <th>Updated profile date</th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+                            @foreach($users as $user)
+                            <tr>
+                                <td>{{$user->id}}</td>
+                                <td>{{$user->username}}</td>
+                                <td>
+                                    <img height="50px" src="{{$user->avatar}}" alt="">
+                                </td>
+                                <td>{{$user->name}}</td>
+                                <td>{{$user->created_at->diffForHumans()}}</td>
+                                <td>{{$user->updated_at->diffForHumans()}}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    @endsection
+
+
+
+    @section('scripts')
+    <!-- Page level plugins -->
+   <script src="{{asset('vendor/datatables/jquery.dataTables.min.js')}}"></script>
+   <script src="{{asset('vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
+
+   <!-- Page level custom scripts -->
+   <script src="{{asset('js/demo/datatables-demo.js')}}"></script>
     @endsection
 
 </x-admin-master>
