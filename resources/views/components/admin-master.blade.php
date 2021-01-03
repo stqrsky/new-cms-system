8991ea40d4dafa4fc55925ddc2d9dc21e97c0087
Starsky <starsky.m@live.com>
2021-01-03 18:31:58 +0100

    Component nesting / organization

--- resources/views/components/admin-master.blade.php
+++ resources/views/components/admin-master.blade.php
@@ -57,10 +57,10 @@
       </div>
 
       <!-- Nav Item - Pages Collapse Menu -->
-      <x-admin-sidebar-posts-links></x-admin-sidebar-posts-links>
+      <x-admin.sidebar.admin-sidebar-posts-links></x-admin.sidebar.admin-sidebar-posts-links>
 
       @if(auth()->user()->userHasRole('admin'))
-          <x-admin-sidebar-users-links></x-admin-sidebar-users-links>
+          <x-admin.sidebar.admin-sidebar-users-links></x-admin.sidebar.admin-sidebar-users-links>
       @endif
 
 
@@ -291,7 +291,7 @@
             <div class="topbar-divider d-none d-sm-block"></div>
 
             <!-- Nav Item - User Information -->
-            <x-admin-top-navbar-user-information></x-admin-top-navbar-user-information>
+            <x-admin.top-nav.admin-top-navbar-user-information></x-admin.top-nav.admin-top-navbar-user-information>
 
           </ul>
 
