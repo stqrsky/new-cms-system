2b9d464e50da07f6049a9dd13bb2c72a082fc145
Starsky <starsky.m@live.com>
2021-01-03 17:30:38 +0100

    comments

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Auth::routes();
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/post/{post}', [App\Http\Controllers\PostController::class, 'show'])->name('post');
 
-
+// reaching pages only through authentication
 Route::middleware('auth')->group(function(){
 
     Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
@@ -21,18 +21,21 @@ Route::middleware('auth')->group(function(){
     Route::patch('/admin/posts/{post}/update', [App\Http\Controllers\PostController::class, 'update'])->name('post.update');
     Route::get('/admin/posts/{post}/edit', [App\Http\Controllers\PostController::class, 'edit'])->name('post.edit');
 
-    Route::get('admin/users/{user}/profile', [App\Http\Controllers\UserController::class, 'show'])->name('user.profile.show');
     Route::put('admin/users/{user}/update', [App\Http\Controllers\UserController::class, 'update'])->name('user.profile.update');
 
     Route::delete('admin/users/{user}/destroy',[App\Http\Controllers\UserController::class, 'destroy'])->name('user.destroy');
 
 });
 
-Route::middleware('role:Admin')->group(function() {
-
+// only users with admin role can see the all users table
+Route::middleware(['role:Admin', 'auth'])->group(function() {
     Route::get('admin/users',[App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+});
 
 
+// ADMIN AND MODEL OWNER -> SAME ACCESS TO PROFILE OWNER / using the UserPolicy
+Route::middleware(['can:view,user'])->group(function() {
+    Route::get('admin/users/{user}/profile', [App\Http\Controllers\UserController::class, 'show'])->name('user.profile.show');
 });
 
 //ALTERNATIV WITH can:
